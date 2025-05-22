aa273c36727cb0ef1fb4bac2f1a300226a2d4102
PrathimaVyas <maddineniprathima@gmail.com>
2025-05-22 19:05:31 +0530

    Update kops.sh

--- kops.sh
+++ kops.sh
@@ -2,17 +2,15 @@
 #export PATH=$PATH:/usr/local/bin/
 #source .bashrc
 
+yum install docker -y
+systemctl start docker
 
-#! /bin/bash
-aws configure
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 wget https://github.com/kubernetes/kops/releases/download/v1.25.0/kops-linux-amd64
 chmod +x kops-linux-amd64 kubectl
 mv kubectl /usr/local/bin/kubectl
 mv kops-linux-amd64 /usr/local/bin/kops
 
-aws s3api create-bucket --bucket cloudanddevopsbyraham0073456.k8s.local --region us-east-1
-aws s3api put-bucket-versioning --bucket cloudanddevopsbyraham0073456.k8s.local --region us-east-1 --versioning-configuration Status=Enabled
-export KOPS_STATE_STORE=s3://cloudanddevopsbyraham0073456.k8s.local
-kops create cluster --name rahams.k8s.local --zones us-east-1a --master-count=1 --master-size t2.medium --node-count=2 --node-size t2.medium
-kops update cluster --name rahams.k8s.local --yes --admin
+export KOPS_STATE_STORE=s3://prathima.mpv.local
+kops create cluster --name prathima.k8s.local --zones us-east-1a,us-east-2a,us-east-3a --master-count=1 --master-size t2.medium --master-volume-size 30 --node-count=2 --node-size t2.micro --node-volume-size 29
+kops update cluster --name prathima.k8s.local --yes --admin
