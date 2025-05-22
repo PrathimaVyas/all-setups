d103c617c9588192b8cf0750473a4702a757c809
PrathimaVyas <maddineniprathima@gmail.com>
2025-05-22 19:09:00 +0530

    Update kops.sh

--- kops.sh
+++ kops.sh
@@ -11,6 +11,6 @@ chmod +x kops-linux-amd64 kubectl
 mv kubectl /usr/local/bin/kubectl
 mv kops-linux-amd64 /usr/local/bin/kops
 
-export KOPS_STATE_STORE=s3://prathima.mpv.local
+export KOPS_STATE_STORE=s3://prathima.mpv.k8s
 kops create cluster --name prathima.k8s.local --zones us-east-1a,us-east-2a,us-east-3a --master-count=1 --master-size t2.medium --master-volume-size 30 --node-count=2 --node-size t2.micro --node-volume-size 29
 kops update cluster --name prathima.k8s.local --yes --admin
