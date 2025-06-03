07c413ddee82637aa12de7ae87fd790413be3e61
PrathimaVyas <maddineniprathima@gmail.com>
2025-06-03 17:21:11 +0530

    Update argocd.sh

--- argocd.sh
+++ argocd.sh
@@ -5,7 +5,7 @@ chmod 700 get_helm.sh
 helm version
 
 #Install ARGOCD Using HELM
-kubectl create namespace argocd
+kubectl create ns argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 kubectl get all -n argocd
 
