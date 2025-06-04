772ba794d0527f9a2979c54ac6fe50ec696c0161
PrathimaVyas <maddineniprathima@gmail.com>
2025-06-04 15:44:38 +0530

    Create prometheus_grafana_setup.sh

---
+++ prometheus_grafana_setup.sh
@@ -0,0 +1,33 @@
+# Install HELM
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
+helm version
+
+# Install Metrics Server
+kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
+kubectl get pods -n kube-system
+kubectl get deployment metrics-server -n kube-system
+
+#️ Add Helm Repos
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm repo add grafana https://grafana.github.io/helm-charts
+helm repo update
+
+#️ Prometheus Setup
+kubectl create ns prometheus
+helm install prometheus prometheus-community/prometheus \
+--namespace prometheus \
+--set alertmanager.persistentVolume.storageClass="gp2" \
+--set server.persistentVolume.storageClass="gp2"
+kubectl get all -n prometheus
+
+# Grafana Setup
+kubectl create ns grafana
+helm install grafana grafana/grafana \
+--namespace grafana \
+--set persistence.storageClassName="gp2" \
+--set persistence.enabled=true \
+--set adminPassword='EKS!sAWSome' \
+--set service.type=LoadBalancer
+
